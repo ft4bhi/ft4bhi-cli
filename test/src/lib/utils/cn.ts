6551734478195ba6350f0f839d77f5749ac72d8b
ft4bhi <psabhiram100@gmail.com>
2025-03-30 10:19:11 +0530

    done

--- test/src/lib/utils/cn.ts
+++
@@ -1,5 +0,0 @@
-// A simple utility for concatenating class names
-export function cn(...classes: (string | undefined | null | false)[]): string {
-    return classes.filter(Boolean).join(" ");
-  }
-  
